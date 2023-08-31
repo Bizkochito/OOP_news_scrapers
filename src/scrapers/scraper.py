f1f87a3273a2c9371a8a4d467fcd641f9de4d899
Gregoire Hupin <hupingregoire@hotmail.com>
2023-08-31 19:05:54 +0200

    Progress

--- src/scrapers/scraper.py
+++ src/scrapers/scraper.py
@@ -2,8 +2,11 @@ import datetime
 import pickle
 import requests
 from bs4 import BeautifulSoup as bs
+from dateutil import parser
 import os
 import json
+import unicodedata
+import ssl
 from functools import partial
 from article import Article
 from task import Task
@@ -19,16 +22,15 @@ class Scraper():
         self.session = requests.Session()
 
     def get_links(self):
-        pass
+        # Output: list or generator object
+        return []
 
     def create_tasks(self):
         return [Task(self, url) for url in self.get_links()]
 
-    def get_article(self):
-        pass
-
-    def write_article(self):
-        pass
+    def get_article(self, task):
+        # Output: Article object expected. Task is acceptable.
+        return task
 
     def describe(self):
         return "Generic scraper"
@@ -80,9 +82,6 @@ class KnackScraper(Scraper):
                        container["source"], 
                        "nl")   
 
-    def write_article(self):
-        pass
-
     def describe(self):
         return "knack.be scraper"
 
@@ -98,11 +97,78 @@ class LesoirScraper(Scraper):
         urls = [url if "//" in url else base_url + url for url in urls]
         return urls
 
-    def get_article(self):
-        pass
+    def get_article(self, task):
 
-    def write_article(self):
-        pass
+        def find_article_title(soup) -> str:
+            article_title = soup.find("h1").text
+            return article_title
+        
+        # selector for geeko.lesoir.be urls
+        def geeko_selector(soup) -> tuple:
+            div = soup.find("div", attrs={"class": "post-content-area"})
+            paragraphs = div.find_all("p")
+            if "Suivez Geeko sur Facebook" in paragraphs[-1].text:
+                paragraphs = paragraphs[:-1]
+            text = ""
+            return paragraphs, text
+        
+        # selector for sosoir.lesoir.be urls
+        def sosoir_selector(soup) -> tuple:
+            h2 = soup.find("h2", attrs={"class": "chapeau"}).text.strip()
+            div = soup.find("div", attrs={"id": "artBody"})
+            paragraphs = div.find_all("p")
+            if "sosoir.lesoir.be" in paragraphs[-1].text:
+                paragraphs = paragraphs[:-1]
+            text = h2
+            return paragraphs, text
+
+
+        # selector for www.lesoir.be and soirmag.lesoir.be urls
+        def lesoirmag_selector(soup) -> tuple:
+            article = soup.find("article", attrs={"class": "r-article"})
+            paragraphs = article.find_all("p")
+            if "www.soirmag.be" in paragraphs[-1].text:
+                paragraphs = paragraphs[:-1]
+            text = ""
+            return paragraphs, text
+
+
+        def find_article_text(soup, url: str) -> str:
+            if "sosoir" in url:
+                paragraphs, text = sosoir_selector(soup)
+            elif "geeko" in url:
+                paragraphs, text = geeko_selector(soup)
+            else:
+                paragraphs, text = lesoirmag_selector(soup)
+            for p in paragraphs:
+                p_text = unicodedata.normalize("NFKD", p.text.strip())
+                text += p_text
+            return text
+
+
+        def find_published_date(soup) -> str:
+            script = soup.find("script", {"type": "application/ld+json"})
+            data = json.loads(script.text, strict=False)
+            try:
+                published_date = data["@graph"][0]["datePublished"]
+            except:
+                published_date = data["datePublished"]
+            date = parser.parse(published_date)
+            return date
+               
+        dict = {"url": task.url}
+        link_response = requests.get(dict["url"])
+        link_soup = bs(link_response.content, "html.parser")
+        print(" Adding article text ...")
+        dict["text"] = find_article_text(link_soup, dict["url"])
+        print(" Adding date ...")
+        dict["date"] = find_published_date(link_soup)
+        print(" Adding title ...")
+        dict["title"] = find_article_title(link_soup)
+        print(" Adding language ...")
+        dict["language"] = "fr"
+        return Article()
+
 
     def describe(self):
         return "lesoir.be scraper"
